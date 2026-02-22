db716643ae47216c1e1200ca6472bc93c10fcf8c
Artitapheiont <87944666+artelephantb@users.noreply.github.com>
2026-02-22 13:28:11 -0500

    Added all posts page to static generator

--- run_staticify.py
+++ run_staticify.py
@@ -3,6 +3,10 @@ from jinja2 import Environment, FileSystemLoader
 import os
 from shutil import rmtree
 
+import tomli
+
+import minify_html
+
 
 # --------------------------------------- #
 # Prepare export folder
@@ -18,15 +22,36 @@ os.mkdir('export')
 # --------------------------------------- #
 # Setup
 # --------------------------------------- #
+with open('configuration.toml', 'rb') as file:
+	site_config = tomli.load(file)
+
+site_name = site_config['site_name']
+footnote = site_config['footnote']
+
 file_system_loader = FileSystemLoader('src/templates')
 environment = Environment(loader=file_system_loader)
 
 
 # --------------------------------------- #
-# Export
+# Export homepage
 # --------------------------------------- #
 template = environment.get_template('homepage.html')
-output = template.render()
+output = template.render(siteName=site_name, user=False, recentPosts=[{'id': 'test', 'title': 'Test'}], permissions=[], footnote=footnote)
 
-with open('export/homepage.html', 'w') as file:
+output = minify_html.minify(output)
+
+with open('export/index.html', 'w') as file:
+	file.write(output)
+
+# --------------------------------------- #
+# Export all posts page
+# --------------------------------------- #
+template = environment.get_template('posts.html')
+output = template.render(siteName=site_name, user=False, permissions=[], footnote=footnote)
+
+output = minify_html.minify(output)
+
+os.mkdir('export/posts')
+
+with open('export/posts/index.html', 'w') as file:
 	file.write(output)
